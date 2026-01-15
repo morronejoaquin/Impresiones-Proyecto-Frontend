0a35fdb7ef7ef76b0ec781c06a92050730d307a2
Joaquin Morrone <morronejoaquinmdq@gmail.com>
2026-01-14 23:57:14 -0300

    Commit: Implementa google maps con datos del backend

--- Workspace/src/app/pages/AboutUs/where-page/where-page.ts
+++ Workspace/src/app/pages/AboutUs/where-page/where-page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import { StoreLocationService } from '../../../services/Location/StoreLocationService';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SafeUrlPipe } from '../pipes/safe-url.pipe';
 @Component({
   selector: 'app-where-page',
-  imports: [],
+  imports: [CommonModule, SafeUrlPipe],
   templateUrl: './where-page.html',
   styleUrl: './where-page.css'
 })
-export class WherePage {
+export class WherePage implements OnInit {
+location:any;
 
+  constructor(private storeService: StoreLocationService) {}
+
+  ngOnInit(){
+    this.storeService.getLocation().subscribe(loc => this.location = loc);
+  }
+
+  get mapUrl(){
+    if(!this.location) return '';
+    return `https://maps.google.com/maps?q=${this.location.lat},${this.location.lng}&z=16&output=embed`;
+  }
 }
